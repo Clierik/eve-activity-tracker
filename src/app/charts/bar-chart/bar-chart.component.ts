cc37042d982d042d36f845e5ee7d167a8509f62a
Clierik <38905813+Clierik@users.noreply.github.com>
2020-08-12 17:59:23 +0300

    added bar chart component

---
+++ src/app/charts/bar-chart/bar-chart.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'ng-chartist';
+// import * as Chartist from 'chartist';
+import {}
+
+@Component({
+  selector: 'app-bar-chart',
+  templateUrl: './bar-chart.component.html',
+  styleUrls: ['./bar-chart.component.scss']
+})
+export class BarChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
