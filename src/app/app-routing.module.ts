4c4aa3e29ec6f052ecbb0bbfcc491cf66c474ca1
Clierik <38905813+Clierik@users.noreply.github.com>
2020-08-12 17:59:57 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home-page', component: HomePageComponent },
+  // { path: '**', component: HomePageComponent } // 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
