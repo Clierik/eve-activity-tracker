0b71fc956efe5d3d4ac9be15bc9e34316d13a6d3
Clierik <38905813+Clierik@users.noreply.github.com>
2020-08-17 19:47:59 +0300

    moved components to appropriate folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { BarChartComponent } from  './charts/bar-chart/bar-chart.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { BarChartComponent } from  './components/charts/bar-chart/bar-chart.component';
 import { HttpService } from './services/http.service';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/app/material-module';
-import { SearchComponentComponent } from './search-component/search-component.component';
+import { SearchComponentComponent } from './components/search-component/search-component.component';
 
 @NgModule({
   declarations: [
